c1b3bb79eccc81822607aed66a27ea7afe9662a3
Augusyyy <8851@holbertonstudents.com>
2024-07-10 00:32:29 +1000

    02 updated

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,7 +1,8 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
+  /* eslint-disable no-unused-vars */
   const self = this;
+  /* eslint-disable no-unused-vars */
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
