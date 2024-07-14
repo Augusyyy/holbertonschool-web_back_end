65673e07404ca72538bacf6b46e0fc1ff723016b
Augusyyy <8851@holbertonstudents.com>
2024-07-15 00:38:02 +1000

    all in

---
+++ ES6_data_manipulation/4-main.js
@@ -0,0 +1,5 @@
+import getListstudents from "./0-get_list_students.js";
+import updateStudentGradeByCity from "./4-update_grade_by_city.js";
+
+console.log(updateStudentGradeByCity(getListstudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+console.log(updateStudentGradeByCity(getListstudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
