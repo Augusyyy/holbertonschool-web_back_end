a8ca2b9778d33fdfc175571ad84e56b6c8fcb326
Augusyyy <8851@holbertonstudents.com>
2024-09-08 00:29:05 +1000

    allinone

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import controllerRouting from 'routes/index.js';
+
+const app = express();
+const port = 1245;
+
+controllerRouting(app);
+
+app.listen(port, () => {});
+
+export default app;
